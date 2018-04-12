2fa4385c564233c8f47c2eb19be0b3a24fb8e458
Chuxin Liao <maxinechuxin@gmail.com>
2018-04-12 18:06:48 +0800

    create proposal

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var bodyParser = require('body-parser');
+var request = require('request');
 var express = require('express');
 var cors = require('cors');
 var app = express();
@@ -17,5 +18,35 @@ app.get('/',function(req,res){
 })
 
 app.post('/create_proposal', function (req, res) {
-    
+	var id = Math.floor(Math.random() * 10000);
+	var proposal = {
+		"$class": "org.acme.biccoins.Proposal",
+		"proposalId": id,
+		"totalCost": req.body.cost,
+		"deptApproval": "false",
+		"vendorApproval": "false",
+		"transferred": "false",
+		"Description": req.body.description,
+		"student": "resource:org.acme.biccoins.Submitter#" + req.body.studentid,
+		"vendor": "resource:org.acme.biccoins.Vendor#" + getVendorID(req.body.vendor),
+		"school": "resource:org.acme.biccoins.GoverningBody#1"
+	};
+	request({
+		url: "http://localhost:3000/api/org.acme.biccoins.Proposal",
+		method: "POST",
+		headers: {"Content-Type": "application/json"},
+		body: JSON.stringify(proposal)
+	}, function(err, response, body) {
+		if (err) {
+			console.log("Error creating proposal: " + err);
+			res.sendStatus(400);
+		} else {
+			console.log("Successfully created proposal " + id);
+			res.sendStatus(200);
+		}
+	});
 })
+
+var getVendorID = (name) => {
+	return 1;
+}
